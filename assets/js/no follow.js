c4b6c4b8530eb915d898a60adf4e5b312e97ddef
trialbymediamjj <1@mjnotinnocent.com>
2025-03-10 16:54:58 +0000

    d

--- assets/js/no follow.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const links = document.querySelectorAll('a');
-  links.forEach(link => {
-    if (link.href.startsWith('http')) {
-      link.setAttribute('rel', 'nofollow noopener');
-    }
-  });
-});
